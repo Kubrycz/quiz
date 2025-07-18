9f5da7ca7b2ebe74c7ea1e0065ec4e61e391e957
paulina.kuberska <p.k.kuberska@gmail.com>
2025-07-18 12:59:00 +0200

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    outDir: "dist", // upewnij się, że to jest 'dist'
+  },
 });
